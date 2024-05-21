66e7a0bd4bc698711831cae9944bbac0cd474505
Suleman Rufai <srufai100@gmail.com>
2024-05-21 13:47:08 +0000

    create a new database correction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- A script to create a new database into mySQL server
 -- Use CREATE DATABASE command to create a new databse system
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
