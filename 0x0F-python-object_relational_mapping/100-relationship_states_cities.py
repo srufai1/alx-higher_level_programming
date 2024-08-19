b8ac219c6f044d4af7a1ebb834ddcfa047832b59
Suleman Rufai <srufai100@gmail.com>
2024-08-19 15:13:23 +0000

    ORM advance update tasks

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
-"""
-Inserts the State "California" with the City "San Francisco"
-into the database hbtn_0e_100_usa.
-"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""List all states"""
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Create engine and bind Base to it
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
-
-    # Create all tables in the database
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
-
-    # Create California state
-    california = State(name="California")
-
-    # Create San Francisco city
-    san_francisco = City(name="San Francisco", state=california)
-
-    # Add California state to the session
-    session.add(california)
-
-    # Commit the changes
+    new_state = State(name="California")
+    session.add(new_state)
     session.commit()
-
-    # Close the session
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
     session.close()
-
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""
-Start link class to table in database
-"""
+"""Lists states"""
+
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from .relationship_city import Base  # Updated import
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
 
 class State(Base):
-    """
-    State class linked to the 'states' table.
-    """
+    """Class representing the states table"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
 
-from .relationship_city import City  # Moved import to the end
-
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)
